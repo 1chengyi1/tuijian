bb546c5c4234eb2b9bd6aac16230be5de4d954ab
1chengyi1 <94302260+1chengyi1@users.noreply.github.com>
2023-06-17 18:09:04 +0800

    推荐.py

--- 推荐.py
+++ 推荐.py
@@ -61,16 +61,14 @@ if uploaded_file is not None:
         st.write(pred)
     st.title("The recommended location for you is：")
     def recommend_movies(pred, data_df=data_df, location_matrix=location_matrix):
-    
-    scores = pd.DataFrame(data_df.groupby('location')['score'].mean())
-    scores['number_of_scores'] = data_df.groupby('location')['score']
-    scores.sort_values('number_of_scores', ascending=False).head(10)
-    AFO_user_score = location_matrix[pred]
-    similar_to_air_force_one=location_matrix.corrwith(AFO_user_score)
-    corr_AFO = pd.DataFrame(similar_to_air_force_one, columns=['correlation'])
-    corr_AFO.dropna(inplace=True)
-    result=corr_AFO.sort_values(by='correlation', ascending=False)
-    similar_location_titles=result[1:4]
-    
-    return similar_location_titles
+        scores = pd.DataFrame(data_df.groupby('location')['score'].mean())
+        scores['number_of_scores'] = data_df.groupby('location')['score']
+        scores.sort_values('number_of_scores', ascending=False).head(10)
+        AFO_user_score = location_matrix[pred]
+        similar_to_air_force_one=location_matrix.corrwith(AFO_user_score)
+        corr_AFO = pd.DataFrame(similar_to_air_force_one, columns=['correlation'])
+        corr_AFO.dropna(inplace=True)
+        result=corr_AFO.sort_values(by='correlation', ascending=False)
+        similar_location_titles=result[1:4]
+        return similar_location_titles
     st.write(similar_location_titles)
