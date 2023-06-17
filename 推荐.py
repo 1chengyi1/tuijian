8ac053262d4b9ff1756bd29bb9e9025c819191b0
1chengyi1 <94302260+1chengyi1@users.noreply.github.com>
2023-06-17 16:29:57 +0800

    推荐.py

--- 推荐.py
+++ 推荐.py
@@ -11,14 +11,12 @@ pathlib.PosixPath=pathlib.WindowsPath
 
 path=os.path.dirname(os.path.abspath(__file__))
 model_path=os.path.join(path,'learn.pkl')
-data_df=pd.read_excel('data2.xlsx', usecols=( 0,1,2,3,4), names=['user','user_id','location_id', 'location','score'])
 learn_inf =load_learner(model_path)
 
 pathlib.PosixPath=temp
 
 st.title("Recommended location App")
 st.write("Upload an image and the app will predict the corresponding label.")
-
 #上传文件
 uploaded_file=st.file_uploader("Choose an image...",type=["jpg","png","jpeg"])
 
@@ -32,36 +30,37 @@ if uploaded_file is not None:
     pred, pred_idx, probs = learn_inf.predict(image)
     
     if pred=="wudadao" :
-        pred=="五大道"
+        pred='五大道'
         st.write(pred)
     elif pred=="tianjinzhiyan" :
-        pred="天津之眼"
+        pred='天津之眼'
         st.write(pred)
     elif pred=="shijizhong" :
-        pred="世纪钟"
+        pred='世纪钟广场'
         st.write(pred)
     elif pred=="gulou":
-        pred="鼓楼"
+        pred='鼓楼'
         st.write(pred)
     elif pred=="cifangzi":
-        pred="瓷房子"
+        pred='瓷房子'
         st.write(pred)
     elif pred=="tianjinhuanlegu":
-        pred="天津欢乐谷"
+        pred='天津欢乐谷'
         st.write(pred)
     elif pred=="shuishanggongyuan":
-        pred="水上公园"
+        pred='水上公园'
         st.write(pred)
     elif pred=="tianjinmeishuguan":
-        pred="天津美术馆"
+        pred='天津美术馆'
         st.write(pred)
     elif pred=="guojiahaiyangbowuguan":
-        pred="国家海洋博物馆"
+        pred='国家海洋博物馆'
         st.write(pred)
     else:
-        pred="西开教堂"
+        pred='西开教堂'
         st.write(pred)
-    st.title("The recommended location for you is:")
+    st.title("The recommended location for you is：")
+    data_df = pd.read_excel('data2.xlsx', usecols=( 0,1,2,3,4), names=['user','user_id','location_id', 'location','score'])
     location_matrix = data_df.pivot_table(index='user_id', columns='location', values='score')
     scores = pd.DataFrame(data_df.groupby('location')['score'].mean())
     scores['number_of_scores'] = data_df.groupby('location')['score']
